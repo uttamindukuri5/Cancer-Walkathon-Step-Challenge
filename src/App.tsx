0fcc0cb548976757f9e81fcd3eb610a5deef577e
uttamindukuri <uttamindukuri5@gmail.com>
2020-09-17 01:14:00 -0500

    feat: added validation to input on registration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Navbar from './components/Header/header';
 import Register from './components/Register/register';
 import RegisterMile from './components/RegisterMile/registerMile';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,20 +15,6 @@ function App() {
         <Register />
         <RegisterMile />
       </section>
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
     </div>
   );
 }
--- src/components/Register/register.tsx
+++ src/components/Register/register.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
 
 import classes from './register.module.css';
 
@@ -11,31 +12,87 @@ export default () => {
         'team 3',
         'team 4'
     ];
-    const [selectedTeam, setSelectedTeam] = useState(teams[0]);
+    const 
+        [selectedTeam, setSelectedTeam] = useState(teams[0]),
+        [ firstName, setFirstName ] = useState(''),
+        [ lastName, setLastName ] = useState(''),
+        [ phone, setPhone ] = useState(''),
+        [ email, setEmail ] = useState(''),
+        [ touched, setTouched ] = useState({
+            firstName: false,
+            lastName: false,
+            phone: false
+        });
+
+    
+    const submit = (): void => {
+        const user = {
+            firstName,
+            lastName,
+            phone, 
+            email,
+            selectedTeam
+        };
+
+        console.log(user);
+        console.log(firstName, lastName, phone, email, selectedTeam);
+    }
+
+    const validatePhone = (phone: string): boolean => {
+        const phoneRegex = /([2-9]\d{9})|([2-9]\d{2}-\d{3}-\d{4})/;
+        return phoneRegex.test(phone);
+    }
 
     return (
-        <div id="form">
+        <div id={ classes.form }>
             <div className={ classes.section }>
-                <label><h6>First Name: </h6></label>
-                <InputText />
+                <label className={ classes.text }><h6>First Name: </h6></label>
+                <InputText 
+                    value={ firstName } 
+                    onChange={ ({ target }: React.ChangeEvent<HTMLInputElement>) => setFirstName(target.value) } 
+                    required={ true }
+                    onClick={ () => setTouched({ ...touched, firstName: true }) }
+                    className={ touched.firstName ? ( firstName.trim().length > 1 ? '' : 'p-invalid') : '' }
+                />
             </div>
             <div className={ classes.section }>
-                <label><strong>Last Name: </strong></label>
-                <InputText />
+                <label className={ classes.text }><strong>Last Name: </strong></label>
+                <InputText 
+                    value={ lastName } 
+                    onChange={ ({ target }: React.ChangeEvent<HTMLInputElement>) => setLastName(target.value) }
+                    required={ true }
+                    onClick={ () => setTouched({ ...touched, lastName: true }) }
+                    className={ touched.lastName ? ( lastName.trim().length > 1 ? '' : 'p-invalid' ) : '' }
+                />
             </div>
             <div className={ classes.section }>
-                <label><strong>Phone: </strong></label>
-                <InputText />
+                <label className={ classes.text }><strong>Phone: </strong></label>
+                <InputText 
+                    value={ phone } 
+                    onChange={ ({ target }: React.ChangeEvent<HTMLInputElement>) => setPhone(target.value) }
+                    required={ true }
+                    onClick={ () => setTouched({ ...touched, phone: true }) }
+                    className={ touched.phone ? ( validatePhone(phone) ? '' : 'p-invalid' ) : '' }
+                />
             </div>
             <div className={ classes.section }>
-                <label><strong>Email (Optional): </strong></label>
-                <InputText />
+                <label className={ classes.text }><strong>Email (Optional): </strong></label>
+                <InputText value={ email } onChange={ ({ target }: React.ChangeEvent<HTMLInputElement>) => setEmail(target.value) }/>
             </div>
             <div className={ classes.section }>
-                <label><strong>Team Name: </strong></label>
-                <Dropdown value={selectedTeam} options={teams} onChange={(e) => setSelectedTeam(e.target.value)} filter showClear
-    placeholder="Select a Team"/>
+                <label className={ classes.text }><strong>Team Name: </strong></label>
+                <Dropdown 
+                    id={ classes.dropdown }
+                    value={selectedTeam} 
+                    options={teams} onChange={(e) => setSelectedTeam(e.target.value)} 
+                    filter 
+                    showClear
+                    placeholder="Select a Team"
+                />
             </div>
+            <div className={ classes.section }>
+                <Button label='Register User' className='p-button-primary' onClick={ submit  }/>
+            </div> 
         </div>
     ); 
 }
