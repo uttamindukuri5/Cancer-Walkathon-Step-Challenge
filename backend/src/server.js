57803e737ca01a8a740526129ff64c0ade0851ea
uttamindukuri <uttamindukuri5@gmail.com>
2020-09-21 20:56:59 -0500

    backend is now https

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
 
 const registrationRouter = require('./routes/registerRouter');
@@ -15,7 +17,10 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     app.use(bodyParser.json());
     app.use('/', queryRouter);
     app.use('/register', registrationRouter);
-    app.listen(4000, () => {
+    https.createServer({
+        key: fs.readFileSync('../../../../../../usr/lib/ssl/ec2-3-137-200-96.us-east-2.compute.amazonaws.com.key'),
+        cert: fs.readFileSync('../../../../../../usr/lib/ssl/ec2-3-137-200-96.us-east-2.compute.amazonaws.com.crt')  
+    }, app).listen(4000, () => {
         console.log('Server has started in port 4000');
     });
 })
